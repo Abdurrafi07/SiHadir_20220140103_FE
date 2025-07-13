6a05f334e42d9796d115d89052a364a49bd14a85
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2025-07-14 00:55:03 +0700

    Membuat absensi request model

---
+++ lib/data/model/kelola/absensi_massal_request.dart
@@ -0,0 +1,29 @@
+class PresensiItem {
+  final int siswaId;
+  final String status;
+
+  PresensiItem({required this.siswaId, required this.status});
+
+  Map<String, dynamic> toJson() => {
+        'siswa_id': siswaId,
+        'status': status,
+      };
+}
+
+class AbsensiMassalRequest {
+  final int jadwalId;
+  final String tanggal;
+  final List<PresensiItem> presensi;
+  final String? fotoPath;
+  final double? latitude;
+  final double? longitude;
+
+  AbsensiMassalRequest({
+    required this.jadwalId,
+    required this.tanggal,
+    required this.presensi,
+    this.fotoPath,
+    this.latitude,
+    this.longitude,
+  });
+}
